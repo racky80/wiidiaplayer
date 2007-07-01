ab55125287ed07900bf1acf463fa20bbef0e9bdf
claude.claude.nl <claude.claude.nl@09a20294-9433-0410-ae9c-9d2bdbf33376>
2007-07-01 16:35:27 +0000

    moved everything together to one project

--- wiidiaserver/convertvideo.sh
+++ wiidiaserver/convertvideo.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 i=0;
-while [ -e "/tmp/convertvideo_fifo_$i" ]; do
+while [ -e "/tmp/convertvideo_fifo_$i" -o -e "/tmp/convertvideo_fifo_2_$i" ]; do
 	i=$(($i+1))
 done
 fifo="/tmp/convertvideo_fifo_$i"
+fifo2="/tmp/convertvideo_fifo_2_$i"
 mkfifo "$fifo";
-/usr/bin/mencoder "$1"  -af resample=44100:0:1 -msglevel all=-1:mencoder=4 -of lavf -lavfopts  format=flv:i_certify_that_my_video_stream_does_not_use_b_frames -af resample=44100:0:1 -af channels=2 -oac mp3lame -lameopts cbr:br=192 -ovc lavc -lavcopts vcodec=flv:vbitrate=500:autoaspect:vratetol=4:keyint=25 -mc 0 -o "$fifo" >/dev/null &
+mkfifo "$fifo2";
+/usr/bin/mencoder "$1" -af resample=44100:0:1 -oac pcm -ovc lavc -lavcopts vcodec=mjpeg:vqscale=0 -vf scale=320:180 -o "$fifo2" >/dev/null &
+/usr/bin/mencoder "$fifo2" -of lavf -lavfopts  format=flv:i_certify_that_my_video_stream_does_not_use_b_frames -af resample=44100:0:1 -af channels=2 -oac mp3lame -lameopts cbr:br=192 -ovc lavc -lavcopts vcodec=flv:vbitrate=10000:autoaspect:vratetol=4:keyint=25 -mc 0 -o "$fifo" >/dev/null &
 cat "$fifo"
 unlink "$fifo"
+unlink "$fifo2"
