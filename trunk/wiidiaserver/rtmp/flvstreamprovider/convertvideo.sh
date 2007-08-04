5fe22b7112f1846caf6763cefa7d0d9f61a7c573
claude.claude.nl <claude.claude.nl@09a20294-9433-0410-ae9c-9d2bdbf33376>
2007-08-04 12:02:13 +0000

    optimised encoding params

--- trunk/wiidiaserver/rtmp/flvstreamprovider/convertvideo.sh
+++ trunk/wiidiaserver/rtmp/flvstreamprovider/convertvideo.sh
@@ -16,7 +16,7 @@ done
 fifo="/tmp/convertvideo_fifo_$i"
 fifo2="/tmp/convertvideo_fifo_2_$i"
 mkfifo "$fifo";
-/usr/bin/mencoder "$1" -af resample=44100:0:1 -oac pcm -ovc lavc -lavcopts vcodec=mjpeg:vqscale=0 -vf scale=320:180 -o "$fifo" > /dev/null&
+/usr/bin/mencoder "$1" -vf scale=320:240 -af resample=44100:0:1 -oac pcm -ovc raw -ofps 15 -o "$fifo" > /dev/null&
 firstcoderpid=$!
 /usr/bin/mencoder "$fifo" -of lavf -lavfopts  format=flv:i_certify_that_my_video_stream_does_not_use_b_frames -af resample=44100:0:1 -af channels=2 -oac mp3lame -lameopts cbr:br=192 -ovc lavc -lavcopts vcodec=flv:vbitrate=2500:autoaspect:vratetol=4:keyint=25 -mc 0 -o "$2" > /dev/null
 unlink "$fifo"
