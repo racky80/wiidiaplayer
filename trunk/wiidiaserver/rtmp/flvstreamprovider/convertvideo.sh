25d1f08eceda6f6c600a6c0e6698e18f87e393ca
claude.claude.nl <claude.claude.nl@09a20294-9433-0410-ae9c-9d2bdbf33376>
2007-08-05 18:38:43 +0000

    new convert params

--- trunk/wiidiaserver/rtmp/flvstreamprovider/convertvideo.sh
+++ trunk/wiidiaserver/rtmp/flvstreamprovider/convertvideo.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-function stop_encoding {
-	secondcoderpid=$!
-	echo "Now killing children $firstcoderpid and $secondcoderpid"
-	kill $firstcoderpid
-	kill $secondcoderpid
-	unlink "$fifo"
+function getmencoderchildids {
+	ps --ppid $$ | awk '$4=="mencoder" {print $1}'
 }
+
+function stop_encoding {
+	echo "now stopping"
+	PIDS="$(getmencoderchildids)"
+	for mypid in $PIDS; do
+		kill $mypid > /dev/null 2&>1;
+	done
+	sleep 2;
+	PIDS="$(getmencoderchildids)"
+	for mypid in $PIDS; do
+		kill -9 $mypid > /dev/null 2&>1;
+	done
+}
+trap stop_encoding TERM;
 trap stop_encoding EXIT;
 
-i=0;
-while [ -e "/tmp/convertvideo_fifo_$i" -o -e "/tmp/convertvideo_fifo_2_$i" ]; do
-	i=$(($i+1))
+FPS=13
+
+/usr/bin/mencoder "$1" -of lavf -lavfopts format=asf:i_certify_that_my_video_stream_does_not_use_b_frames -vf scale=320:240 -af resample=44100:0:1 -oac pcm -ovc raw -ofps $FPS -o /dev/fd/3 3>&1 >/dev/null 2&>1 | /usr/bin/mencoder /dev/stdin -of lavf -lavfopts  format=flv:i_certify_that_my_video_stream_does_not_use_b_frames -af resample=44100:0:1 -af channels=2 -oac mp3lame -lameopts cbr:br=192 -ovc lavc -lavcopts vcodec=flv:vbitrate=2500:autoaspect:vratetol=4:keyint=25 -mc 0 -ofps $FPS -o "$2" > /dev/null 2&>1 &
+while [ -n "$(getmencoderchildids)" ]; do
+	sleep 1;
 done
-fifo="/tmp/convertvideo_fifo_$i"
-fifo2="/tmp/convertvideo_fifo_2_$i"
-mkfifo "$fifo";
-/usr/bin/mencoder "$1" -vf scale=320:240 -af resample=44100:0:1 -oac pcm -ovc raw -ofps 15 -o "$fifo" > /dev/null&
-firstcoderpid=$!
-/usr/bin/mencoder "$fifo" -of lavf -lavfopts  format=flv:i_certify_that_my_video_stream_does_not_use_b_frames -af resample=44100:0:1 -af channels=2 -oac mp3lame -lameopts cbr:br=192 -ovc lavc -lavcopts vcodec=flv:vbitrate=2500:autoaspect:vratetol=4:keyint=25 -mc 0 -o "$2" > /dev/null
-unlink "$fifo"
