1f65ad9ff66221dbfc77a0f7dd762948a9926145
claude.claude.nl <claude.claude.nl@09a20294-9433-0410-ae9c-9d2bdbf33376>
2007-08-28 21:42:00 +0000

    some optimizations

--- wiidiaserver/rtmp/flvstreamprovider/convertvideo.sh
+++ wiidiaserver/rtmp/flvstreamprovider/convertvideo.sh
@@ -19,17 +19,21 @@ function stop_encoding {
 trap stop_encoding TERM;
 trap stop_encoding EXIT;
 
-FPS=13
+FPS=17
 
-/usr/bin/mencoder "$1" \
-				-of lavf -lavfopts format=asf:i_certify_that_my_video_stream_does_not_use_b_frames \
-                -oac pcm -ovc raw \
-                -vf scale=320:240 -af resample=44100:0:1 -ofps $FPS \
-                -o /dev/fd/3 3>&1 >/dev/null 2&>1 | \
-       /usr/bin/mencoder /dev/stdin \
-				-of lavf -lavfopts  format=flv:i_certify_that_my_video_stream_does_not_use_b_frames \
-                -af resample=44100:0:1 -af channels=2 -oac mp3lame -lameopts cbr:br=192 -mc 0 \
-                -ovc lavc -lavcopts vcodec=flv:vbitrate=2500:autoaspect:vratetol=4:keyint=25 -ofps $FPS -o "$2" > /dev/null 2&>1 &
+nice -n 2 /usr/bin/mencoder "$1" \
+				-of lavf -lavfopts format=asf \
+                -oac pcm -af resample=44100:0:1 \
+                -ovc raw -vf scale=400:224 -ofps $FPS \
+                -o /dev/fd/3 3>&1 >/var/log/mencoder/1 2>&1 | \
+        nice -n 1 /usr/bin/mencoder /dev/stdin \
+        		-of lavf -lavfopts format=flv \
+        		-af resample=44100:0:1 -af channels=2 -oac mp3lame -lameopts cbr:br=128 -mc 0 \
+        		-ovc lavc -lavcopts vcodec=flv:vbitrate=2500:autoaspect:vratetol=1000:keyint=1 -ofps $FPS \
+        		-o "$2" > /var/log/mencoder/2 2>&1 &
+
+
 while [ -n "$(getmencoderchildids)" ]; do
+	echo "$(getmencoderchildids)"
 	sleep 1;
 done
