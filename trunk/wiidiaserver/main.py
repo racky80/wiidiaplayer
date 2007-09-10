7386b6dcea24a9ddc31db423b1138c3f846d883c
dubbeld00 <dubbeld00@09a20294-9433-0410-ae9c-9d2bdbf33376>
2007-09-10 18:35:00 +0000

    Added: - Daemonize code - Help menu (-h)   - Version info (-v)   - Root dir changing (-r <rootdir>)
    Working on:
    - pid saving (and killing old process)
    - mediadir (-m) setting

--- trunk/wiidiaserver/main.py
+++ trunk/wiidiaserver/main.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python 
+
 from twisted.web import server, resource, static
 from twisted.internet import reactor, protocol, error
-import os, urllib, sys, logging
+import os, urllib, sys, logging, getopt
 import rtmp
 
 sys.path.append(os.path.dirname(__file__));
@@ -102,21 +104,119 @@ class GetFile(WiiServerExternalprogramResource):
     def getProcessPassthroughClass(self):
         return ProcessPassthroughprotocol
     
-logging.basicConfig(level=logging.INFO)
-try:
-    rootdir = sys.argv[1]
-except IndexError:
+def print_help():
+    # Some help information
+    print "Usage: main.py -m <mediadir> [-dvh] [-r <rootdir>]"
+    print
+    print "Options:"
+    print "  -d   fork daemon process"
+    print "  -v   print version information"
+    print "  -r   root directory"
+    print "  -m   media directory"
+    print "  -h   print this message"
+
+def print_version():
+    # Here you are... Do with it whatever you want
+    print "Wiidiaplayer v0.0.0.5"
+    print
+    print "Your copyrights here ;)"
+
+
+def daemonize():
+    try:
+        pid = os.fork() 
+        if pid > 0:
+            sys.exit(0) 
+    except OSError:
+        print "fork() failed"
+        sys.exit(1)
+    
+    # Might consider changing this part:    
+    os.chdir(os.path.dirname(__file__))
+    os.setsid()
+    os.umask(0)
+    
+    # Try to get the pid, if it fails exit
+    try:
+        pid = os.fork()
+        if pid > 0:
+            sys.exit(0) 
+    except OSError:
+        print "fork() failed"
+        sys.exit(1)
+    
+    # You might want to save the pid to /var/run/<name>.pid, you can deside wheter or not do that
+    #pidFile = 
+    
+    # Write keyboard input to nothing
+    dev_null = file('/dev/null', 'r')
+    os.dup2(dev_null.fileno(), sys.stdin.fileno())
+    dev_null.close()
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hdvr:m:")
+    except getopt.GetoptError:
+        print_help()
+        sys.exit(2)
+    
+    fork = False
     rootdir = "root"
+    mediadir = ""
+    
+    # Check what command we should call
+    for o, a in opts:
+        if o == '-d':
+            fork = True
+        if o == '-h':
+            print_help()
+            sys.exit()
+        if o == '-v':
+            print_version()
+            sys.exit()
+        if o == '-r':
+            rootdir = a
+        if o == '-m':
+            mediadir = a
+    
+    # Currently checking what's the best way to pass mediadir through to rtmp
+    #if mediadir:
+        # Send it to the scripts who needs this dir
+    #    mediadir = mediadir
+    #else:
+    #    print_help()
+    #    sys.exit(0)
+    
+    # We are going to daemonize, first only save the logs:
+    if fork:
+        try:
+            sys.stderr.fileno
+            sys.stdout.fileno
+            # Now we save it to /var/log/wiidiaplayer
+            out_log = file('/var/log/wiidiaplayer', 'a+', 0)
+            sys.stderr.flush()
+            sys.stdout.flush()
+            os.dup2(out_log.fileno(), sys.stderr.fileno())
+            os.dup2(out_log.fileno(), sys.stdout.fileno())
+            out_log.close()
+            # Now daemonize it all:
+            daemonize()
+        except AttributeError:
+            pass
+    
+    # Here is your normal code...
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Using %s as rootdir" %rootdir)
+    
+    root = static.File(rootdir)
+    root.putChild('getdir',GetDir())
+    root.putChild('getfile',GetFile())
+    root.putChild('getinfo',GetInfo())
+    root.putChild('feedback',PrintFeedback())
+    site = server.Site(root)
+    reactor.listenTCP(1935, rtmp.RTMPServerFactory( ))
+    reactor.listenTCP(8080, site)
+    reactor.run()
 
-logging.info("Using %s as rootdir" %rootdir);
-
-
-root = static.File(rootdir)
-root.putChild('getdir',GetDir())
-root.putChild('getfile',GetFile())
-root.putChild('getinfo',GetInfo())
-root.putChild('feedback',PrintFeedback())
-site = server.Site(root)
-reactor.listenTCP(1935, rtmp.RTMPServerFactory( ))
-reactor.listenTCP(8080, site)
-reactor.run()
+if __name__ == '__main__':
+    main()
