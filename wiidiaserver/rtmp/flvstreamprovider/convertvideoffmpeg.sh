7f2df4e63d0c21ffbf7a5f6d990635bc4cab2262
claude.claude.nl <claude.claude.nl@09a20294-9433-0410-ae9c-9d2bdbf33376>
2008-01-20 01:21:06 +0000

    FFMpeg shouldn't hang anymore, mplayer shouldn't stop after 1 second anymore, returned to mencoder being the default encoding method again

--- wiidiaserver/rtmp/flvstreamprovider/convertvideoffmpeg.sh
+++ wiidiaserver/rtmp/flvstreamprovider/convertvideoffmpeg.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+function getffmpegchildids {
+	ps --ppid $$ | awk '$4=="ffmpeg" {print $1}'
+}
+
+function stop_encoding {
+	echo $$ > /tmp/proc_$$
+	echo
+	echo "now stopping"
+	echo
+	echo
+	PIDS="$(getffmpegchildids)"
+	for mypid in $PIDS; do
+		kill $mypid > /dev/null 2>&1;
+	done
+	sleep 2;
+	PIDS="$(getffmpegchildids)"
+	for mypid in $PIDS; do
+		kill -9 $mypid > /dev/null 2>&1;
+	done
+}
+trap stop_encoding TERM;
+trap stop_encoding EXIT;
 
 FPS=16
-nice -n 1 ffmpeg -y -i "$1" -ar 44100 -ab 128k -b 400k -r $FPS -f flv -s 400x224 -ac 1 "$2" 2>&1
+nice -n 1 ffmpeg -y -i "$1" -ar 44100 -ab 128k -b 400k -r $FPS -f flv -s 400x224 -ac 1 "$2" 2>&1 &
+
+sleep 5
+
+while [ -n "$(getffmpegchildids)" ]; do
+	sleep 1;
+done
